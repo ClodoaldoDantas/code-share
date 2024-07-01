9ae7f8628b504b9202e943330f1991a0731858fa
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-01 14:33:26 -0300

    Add authentication and database configuration files

--- src/components/user-nav/user-info.tsx
+++ src/components/user-nav/user-info.tsx
@@ -5,6 +5,8 @@ import { useSession } from 'next-auth/react'
 export function UserInfo() {
   const { data: session } = useSession()
 
+  console.log(session)
+
   return (
     <div className="flex flex-col space-y-1">
       <p className="text-sm font-medium leading-none">{session?.user?.name}</p>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,17 @@
 import { AuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+import { Adapter } from 'next-auth/adapters'
+
+const prisma = new PrismaClient()
+
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
