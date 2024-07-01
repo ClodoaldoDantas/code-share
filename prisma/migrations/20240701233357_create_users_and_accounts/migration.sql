b3fed5ae4fdd803ddc3876f42f42db5572dea2d1
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-01 20:35:15 -0300

    Add user and account tables to the database schema

--- prisma/migrations/20240701233357_create_users_and_accounts/migration.sql
+++ prisma/migrations/20240701233357_create_users_and_accounts/migration.sql
@@ -30,32 +30,8 @@ CREATE TABLE "accounts" (
     CONSTRAINT "accounts_pkey" PRIMARY KEY ("provider","provider_account_id")
 );
 
--- CreateTable
-CREATE TABLE "sessions" (
-    "session_token" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "verificationtokens" (
-    "identifier" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "verificationtokens_pkey" PRIMARY KEY ("identifier","token")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
--- CreateIndex
-CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
-
 -- AddForeignKey
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
