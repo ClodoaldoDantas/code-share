ed20807857e23ad3f82876c5e4943c257559f505
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-15 15:00:50 -0300

    Remove snippets and update snippets table, create users, accounts, and snippets tables

--- prisma/migrations/20240715175841_create_users_accounts_snippets/migration.sql
+++ prisma/migrations/20240715175841_create_users_accounts_snippets/migration.sql
@@ -30,8 +30,23 @@ CREATE TABLE "accounts" (
     CONSTRAINT "accounts_pkey" PRIMARY KEY ("provider","provider_account_id")
 );
 
+-- CreateTable
+CREATE TABLE "snippets" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "technology" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "snippets_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "snippets" ADD CONSTRAINT "snippets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
