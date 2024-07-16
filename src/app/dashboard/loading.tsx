53c275500788666bb76553a90e606183ab4bb7f1
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-07-16 13:38:52 -0300

    Refactor grid layout in DashboardLoading component

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 export default function DashboardLoading() {
   return (
     <main className="max-w-5xl mx-auto px-4 my-5">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid gap-4 md:grid-cols-3">
         <Skeleton className="h-[188px] rounded-lg" />
         <Skeleton className="h-[188px] rounded-lg" />
         <Skeleton className="h-[188px] rounded-lg" />
